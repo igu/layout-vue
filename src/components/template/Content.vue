da9f0dfc5403db0b0c7ab8ae88307af0adf8d0be
Igor Alisson <igoralissoncn@gmail.com>
2019-09-26 01:04:45 -0300

    Configurando toggleMenu e importando bootstrap

---
+++ src/components/template/Content.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="content">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Content'
+}
+</script>
+
+<style>
+    .content {
+        grid-area: content;
+        background-color: #F5F5F5;
+        padding: 20px;
+    }
+</style>
---
+++ src/components/template/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="footer">
+        <span><strong>Segurança - Administração - Direitos Reservados</strong></span>
+    </footer>
+</template>
+
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #F5F5F5;
+        color: #333;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
